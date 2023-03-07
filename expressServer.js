1b7ca9210506a51a2fb4e92d85844a673144b14f
Javaney <javaneyt@icloud.com>
2023-03-07 14:27:50 -0600

    made changes to ID and GET req for one element

--- expressServer.js
+++ expressServer.js
@@ -15,7 +15,7 @@ const pool = dbConn.getPool();
 
 
   
-app.get('/onepiece', (req, res, next) => {
+app.get('/api/onepiece', (req, res, next) => {
     // Get all the rows in onepiece table
     pool.query('SELECT * FROM onepiece', (err, result) => {
       if (err){
@@ -27,6 +27,31 @@ app.get('/onepiece', (req, res, next) => {
       return res.send(rows);
     });
 });
+
+//NOW CREATE A GET REQUEST FOR ONE ELEMENT OUT YOUR TABLE MY GUY
+app.get('/api/onepiece/:id', (req, res, next) => {
+  // Get a single person from the table
+  let id = Number.parseInt(req.params.id);
+  if (!Number.isInteger(id)){
+    res.status(404).send("No person found with that ID");
+  }
+  console.log("person ID: ", id);
+
+  pool.query('SELECT * FROM onepiece WHERE id = $1', [id], (err, result) => {
+    if (err){
+      return next(err);
+    }
+
+    let pirate = result.rows[0];
+    console.log("Single Pirate ID", id, "values:", pirate);
+    if (pirate){
+      return res.send(pirate);
+    } else {
+      return res.status(404).send("No pirate found with that ID");
+    }
+  });
+});
+
 
 
 app.listen(port, () => {
